c74cb5e62b56413e7bdda0145c59c37458d64811
keshibat <kensukeshibata@.com>
2019-03-05 12:53:23 +1100

    Add quiz and aswer as a variable

---
+++ madlib_game.rb
@@ -0,0 +1,18 @@
+#Fill in Blank Quiz for Coder Academy Project1
+
+# A list of replacement words to be passed in to the play game function.
+blanks  = ["___1___", "___2___" , "___3___", "___4___"]
+
+# Script for this quiz
+quiz1 = "Agent: I'm sorry, we have no mid-size available at the moment.\n
+Jerry: I don't understand, I made a reservation, do you have my reservation?\n
+Agent: Yes, we do, unfortunately we ran out of cars.\n
+Jerry: But the ___1___ keeps the car here. That's why you have the reservation.\n
+Agent: I know why we have reservations.\n
+Jerry: I don't think you do. If you did, I'd have a car.See, you know how to ___2___ the reservation,
+       you just don't know how to ___3___ the reservation and
+       that's really the most important part of the reservation, the ___4___.
+       Anybody can just take them."
+
+#answers for quiz1
+answer1 = ["reservation", "take", "hold", "holding"]
