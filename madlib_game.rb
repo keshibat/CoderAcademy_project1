005aedd4916dd996671b895a534ba7a00f20220c
sjwconway <sjwconway@hotmail.com>
2019-03-06 13:32:55 +1100

    points_system

--- madlib_game.rb
+++ madlib_game.rb
@@ -52,12 +52,12 @@ def guideline()
      puts instructions
      puts "            good luck!".upcase.green
      puts "\nWelcome to Seinfeld Quiz!!"
-     sleep 3
+     sleep 1
      puts word_iteration(quiz, "Jerry:", :blue)
   else
     puts "            good luck!".upcase.green
     puts "\nWelcome to Seinfeld Quiz!!"
-    sleep 3
+    sleep 1
     puts word_iteration(quiz, "Jerry:", :blue)
   end
 end
@@ -65,8 +65,8 @@ guideline()
 
 
 
+#method for this fill in blank quiz
 def madlib_game()
-  puts "\nWelcome to Seinfeld Quiz!!"
   #Array:lists of replacement word to be passed in to the madlib_game method
   blanks  = ["___1___", "___2___" , "___3___", "___4___"]
   #Array: lists of answers to be matched with variable called blanks
@@ -74,35 +74,85 @@ def madlib_game()
   #Limit maximum the user attempt as 3 for each blanks
   attempts = 3
   player_blank = 0
+  score = 0
   #Starts with 0 loop through unitl the end of Array of blanks
   while player_blank < blanks.length
       #pirnt which blank question  reffering to
-      puts "\nWhat is answer to" "\n"+ blanks[player_blank] + "?" "\n"
+      puts "\nWhat is the answer to" "\n"+ blanks[player_blank].yellow + "?" "\n"
       #Getting user input for quize answers
-      player_answer = gets.chomp
+      player_answer = gets.chomp.downcase
   # if user's input mathces answers print "You're correct! Keep going!!"
   if player_answer == answer1[player_blank]
-    puts "You're correct! Keep going!!"
+    score +=5
+    puts "You're correct! Keep going!! chances left (#{attempts}) you have #{score} points".green
     # if player got correct answer, increment player_blank so that player can go next question
     player_blank += 1
     # if player reached the end of blanks of question in this case "___4___"
     # and got correct answer, eixt from terminal
-    if player_blank == blanks.length
-      puts "Congrats! You're Seinfeld master"
-      exit
+    if player_blank == blanks.length and attempts == 3
+      score *= attempts
+      puts "Congrats! You're Seinfeld master!!!!!, You filled all blanks without losing a chance! and scored #{score} points!".upcase.yellow
+      puts"https://www.youtube.com/watch?v=A7uvttu8ct0 ".blue + "Ctrl + click on link to watch :)"
+      sleep 3
+      puts "Would You Like To Play again? ('y' to play again/ any other key to exit)"
+      user_input = gets.chomp.strip.downcase
+      if user_input == "y"
+        puts word_iteration(quiz, "Agent:", :blue)
+        madlib_game
+      else
+        puts "Thanks for playing!".green
+        exit
+      end
+    elsif player_blank == blanks.length and attempts == 2
+      score *= attempts
+      puts "Congrats! You know your Seinfeld, you filled all blanks with #{attempts} chances remaining! and scored #{score} points!".yellow
+      sleep 3
+      puts "Would You Like To Play again? ('y' to play again/ any other key to exit)"
+      user_input = gets.chomp.strip.downcase
+      if user_input == "y"
+        puts word_iteration(quiz, "Agent:", :blue)
+        madlib_game
+      else
+        puts "Thanks for playing!".green
+        exit
+      end
+    elsif player_blank == blanks.length and attempts == 1
+      score *= attempts
+      puts "Congrats! you could sharpen up on your Seinfeld knowledge!, you filled all blanks with #{attempts} chances remaining! and scored #{score} points!".yellow
+      sleep 3
+      puts "Would You Like To Play again? ('y' to play again/ any other key to exit)"
+      user_input = gets.chomp.strip.downcase
+      if user_input == "y"
+        puts word_iteration(quiz, "Agent:", :blue)
+        madlib_game
+      else
+        puts "Thanks for playing!".green
+        exit
+      end
+
     end
     #if player got wrong answer decrement of number of attempts
-
-
   else attempts -= 1
-    puts "Wrong answer. Try again"
+    puts "Wrong answer. Try again, chances left (#{attempts})".red
     #if player reached maximum attempts exit from the game
     if attempts == 0
-      puts "Sorry, max number of attempts reached. Program closed......"
+      puts "Sorry, you don't know Seinfeld..."
+      puts "Would you like to try again? ('y' to play again/ any other key to exit)"
+      user_input = gets.chomp.strip.downcase
+      if user_input == "y"
+        puts "            good luck!".upcase.green
+sleep 3
+        puts word_iteration(quiz, "Agent:", :red)
+        madlib_game
+      else
+      puts "thanks for playing!".green
       exit
     end
   end
   end
 end
 
-madlib_game()
+
+end
+
+madlib_game
