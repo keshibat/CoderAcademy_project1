f92cd47d4f40f91270733d275d579cf702049b89
keshibat <kensukeshibata@.com>
2019-03-27 15:48:55 +1100

    Add comment for structuring MVC

--- madlib_game.rb
+++ madlib_game.rb
@@ -1,5 +1,46 @@
 #Fill in Blank Quiz for Coder Academy Project1
 
+#MVC
+
+#Model
+##script of the quiz1
+##Script1(Car Reservation)
+##"I'm sorry, we have no mid-size available at the moment.
+## ---     -----    ---    ----    ------   ------   ----
+## ---     -----    ---    ----Anybody can just take them."
+
+##Scrit of the quiz2
+## Hi, would you be interested in switching over to TMI long distance service?
+## ---     -----    ---    ----    ------   ------   ----
+## ---     -----    ---    ----
+## ---     -----    ---    ----Jerry: Well, now you know how I feel.
+
+
+## blanks  = ["___1___", "___2___" , "___3___", "___4___"]
+
+## answer1 = ["reservation", "take", "hold", "holding"]
+
+
+#View
+#terminal screen itself
+
+#Controller (Octopus) (View Model)
+##Connect view and Controller
+##updates the view(terminal screen itself) when the model(script of the quiz) changes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # variable called quiz1
 quiz1 = "\nAgent: I'm sorry, we have no mid-size available at the moment.\n
 Jerry: I don't understand, I made a reservation, do you have my reservation?\n
@@ -10,6 +51,8 @@ Jerry: I don't think you do. If you did, I'd have a car.See, you know how to ___
        you just don't know how to ___3___ the reservation and
        that's really the most important part of the reservation, the ___4___.
        Anybody can just take them."
+
+
 
 puts quiz1
 
